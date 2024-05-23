d19036a7758725b26358fb56a35cafc486d954da
jairo <jairodeavila2004@gmail.com>
2024-05-22 22:53:24 -0500

    actu

---
+++ Navigation.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { StatusBar, SafeAreaView } from 'react-native';
+import CameraComponent from './components/CameraComponent';
+import RegisterScreen from './screens/RegisterScreen';
+import LoginScreen from './screens/LoginScreen';
+import HomeScreen from './screens/HomeScreen';
+import Settingcreen from './screens/SettingScreen';
+import ComponentPantallas from './components/ComponentPantallas';
+import UserScreen from './screens/UserScreen';
+import CalificanosScreen from './screens/CalificanosScrenn';
+import NovedadScreen from './screens/NovedadScreen';
+import RegisterObjets from './screens/RegisterObjets';
+import EditarObjets from './screens/EditarObjets';
+import Inventario from './screens/Inventario';
+import DeleScreen from './screens/DeleScreen';
+import ReportScreen from './screens/ReportScreen';
+import RecupContrScreen from './screens/RecupContrScreen';
+import DetailScreen from './screens/DetailScreen';
+import VerToken from './utils/ComponentToken';
+
+const Stack = createStackNavigator();
+
+const Navigation = () => {
+  return (
+    <SafeAreaView style={{ flex: 1, paddingTop: StatusBar.currentHeight }}>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Home">
+        <Stack.Screen name="Details" component={DetailScreen} />
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Camera" component={CameraComponent} />
+          <Stack.Screen name="Token" component={VerToken} />
+          <Stack.Screen name="Setting" component={Settingcreen} />
+          <Stack.Screen name="Register" component={RegisterScreen} />
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="componente" component={ComponentPantallas} />
+          <Stack.Screen name="User" component={UserScreen} />
+          <Stack.Screen name="Calificanos" component={CalificanosScreen} />
+          <Stack.Screen name="Novedad" component={NovedadScreen} />
+          <Stack.Screen name="RegisterObjets" component={RegisterObjets} />
+          <Stack.Screen name="EditarObjets" component={EditarObjets} />
+          <Stack.Screen name="Inventario" component={Inventario} />
+          <Stack.Screen name="DeleScreen" component={DeleScreen} />
+          <Stack.Screen name="ReportScreen" component={ReportScreen} />
+          <Stack.Screen name="RecupContrScreen" component={RecupContrScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaView>
+  );
+};
+
+export default Navigation;
---
+++ screens/LoginScreen.jsx
@@ -0,0 +1,110 @@
+import React, { useState } from 'react';
+import { View, StyleSheet, TextInput, Button, Text, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+
+const LoginScreen = () => {
+  const navigation = useNavigation();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleLogin = async () => {
+    try {
+      const response = await fetch('http://192.168.1.38:3000/usuario/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+        }),
+      });
+
+
+      const data = await response.json();
+
+      if (response.ok) {
+        const token = data.token;
+        await AsyncStorage.setItem('token',token);
+        // Si la respuesta es exitosa, redirige al usuario a la pantalla de inicio
+        navigation.navigate('Home');
+      } else {
+        // Si hay un error en la respuesta, muestra un mensaje de error
+        setErrorMessage(data.message || 'Ocurrió un error durante el inicio de sesión.');
+      }
+    } catch (error) {
+      // Si hay un error en la solicitud, muestra un mensaje de error
+      console.error('Error de red:', error);
+      setErrorMessage('Ocurrió un error de red. Por favor, intenta nuevamente.');
+    }
+  };
+
+  const handleRegisterLink = () => {
+    navigation.navigate('Register');
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.tittle}>Inicio De Sesión</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Correo electrónico"
+        onChangeText={(text) => setEmail(text)}
+        value={email}
+        keyboardType="email-address"
+        autoCapitalize="none"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Contraseña"
+        onChangeText={(text) => setPassword(text)}
+        value={password}
+        secureTextEntry
+        autoCapitalize="none"
+      />
+      {errorMessage ? <Text style={styles.errorText}>{errorMessage}</Text> : null}
+      <TouchableOpacity onPress={handleRegisterLink}>
+        <Text style={styles.registerLink}>¿No tienes cuenta? Regístrate</Text>
+      </TouchableOpacity>
+      <Button title="Iniciar Sesión" onPress={handleLogin} color={'#39A900'} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 16,
+  },
+  input: {
+    marginBottom: 16,
+    padding: 8,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 4,
+  },
+  errorText: {
+    color: 'red',
+    marginBottom: 16,
+  },
+  registerLink: {
+    marginBottom: 16,
+    textAlign: 'center',
+    color: '#39A900',
+    textDecorationLine: 'underline',
+  },
+  tittle: {
+    marginBottom: 32,
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#39A900',
+    textAlign: 'center',
+  },
+});
+
+export default LoginScreen;
